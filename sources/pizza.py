6ab801c6fc451ccb8f4a6202f4cdd210953717c8
Frederic Desmars <f.desmars@wanadoo.fr>
2021-12-08 11:00:03 +0100

    coreection syntaxe maker.py et pizza.py

--- sources/pizza.py
+++ sources/pizza.py
@@ -22,26 +22,7 @@ RECIPES = {
         'ingredients': {'mozzarella': 3, 'pepper': 4, 'onion': 3},
         'price': 11,
     },
-    'margherita': {
-        'ingredients': {'mozzarella': 2, 'oregano': 4},
-        'price': 10,
-        'sauce': 'tomato'
-    },
-    'quattro fromagi': {
-        'ingredients': {'mozzarella': 2, 'parmesan': 2, 'gorgonzola': 2, 'cheddar': 2, 'oregano': 3},
-        'price': 12,
-        'sauce': 'tomato'
-    },
-    'crudo': {
-        'ingredients': {'mozzarella': 2, 'ham': 3, 'basil': 3, 'mushroom': 3},
-        'price': 11,
-        'sauce': 'tomato'
-    },
-    'pugliese': {
-        'ingredients': {'mozzarella': 4, 'oregano': 2, 'onion': 5, 'mushroom': 2},        
-        'price': 11,
-        'sauce': 'tomato'
-    },
+    
     'romana': {
         'ingredients': {'ham': 4, 'olive': 3, 'onion': 2, 'basil': 2},
         'price': 12,
@@ -64,7 +45,28 @@ RECIPES = {
     'my pizza': {
         'ingredients': {'mozzarella': 3, 'garlic': 5},
         'price': 10,
-    }
+    },
+
+    'margherita': {
+        'ingredients': {'mozzarella': 2, 'oregano': 4},
+        'price': 10,
+        'sauce': 'tomato'
+    },
+    'quattro fromagi': {
+        'ingredients': {'mozzarella': 2, 'parmesan': 2, 'gorgonzola': 2, 'cheddar': 2, 'oregano': 3},
+        'price': 12,
+        'sauce': 'tomato'
+    },
+    'crudo': {
+        'ingredients': {'mozzarella': 2, 'ham': 3, 'basil': 3, 'mushroom': 3},
+        'price': 11,
+        'sauce': 'tomato'
+    },
+    'pugliese': {
+        'ingredients': {'mozzarella': 4, 'oregano': 2, 'onion': 5, 'mushroom': 2},        
+        'price': 11,
+        'sauce': 'tomato'
+    },
 }
 
 
